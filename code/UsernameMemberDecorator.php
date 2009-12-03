66e29b968b34756439a0dc12f5e0e7bfd103ee9b
Jeremy Shipman <jeremy@burnbright.net>
2009-12-03 04:37:29 +0000

    username generation

--- code/UsernameMemberDecorator.php
+++ code/UsernameMemberDecorator.php
@@ -2,27 +2,25 @@
 
 class UsernameMemberDecorator extends DataObjectDecorator{
 	
-		function extraStatics(){
-			return array(
-				'db' => array(
-					'Username' => 'Varchar'
-				)
-			);
-		}
+	function extraStatics(){
+		return array(
+			'db' => array(
+				'Username' => 'Varchar'
+			)
+		);
+	}
 	
 	/**
 	 * Generates a username in the form: FSurname , where F is the first letter of the member's first name
 	 * More of the first name is added if that username is already taken
 	 */
 	function generateUsername(){
-		$member = $this->owner();
-		
+		$member = $this->getOwner();	
 		$count = 1;
 		do{
-			$username = strtolower(substr($member->FirstName(),0,$count).$member->Surname());
-			$count++;
-		}while(!DataObject::get_one('Member',"Username = '$username'"));
-		
+			$username = strtolower(substr($member->FirstName,0,$count).$member->Surname);
+			//if($count > 0) $username .= $count;
+		}while(DataObject::get_one('Member',"Username = '$username'"));
 		return $username;
 	}	
 }
